93929f4c1c5933a588ab24c62055805cf37be4a7
Azat777-user <mardanow.azat2016@yandex.ru>
2020-02-07 14:13:43 +0300

    paginator tdd

---
+++ src/components/Users/User.js
@@ -0,0 +1,58 @@
+import React from "react";
+import userPhoto from "../../assets/images/user-photo.png";
+import {NavLink} from "react-router-dom";
+import s from "./Users.module.css";
+
+
+let User = ({user, followingInProgress, unfollow, follow, key}) => {
+
+  return (
+    <div key={key} className={s.wrapper}>
+      <span>
+        <div className={s.image}>
+          <NavLink to={"/profile/" + user.id}>
+            <img
+              src={
+                user.photos.small != null ? user.photos.small : userPhoto
+              }
+              alt=""
+            />
+          </NavLink>
+        </div>
+        <div>
+          {user.followed ? (
+            <input
+              disabled={followingInProgress.some(id => id === user.id)}
+              type="submit"
+              value="Unfollow"
+              onClick={() => {
+                unfollow(user.id);
+              }}
+            />
+          ) : (
+            <input
+              disabled={followingInProgress.some(id => id === user.id)}
+              type="submit"
+              value="Follow"
+              onClick={() => {
+                follow(user.id);
+              }}
+            />
+          )}
+        </div>
+      </span>
+      <span>
+        <span>
+          <div>{user.name}</div>
+          <div>{user.status}</div>
+        </span>
+        <span>
+          <div>{"user.location.country"}</div>
+          <div>{"user.location.city"}</div>
+        </span>
+      </span>
+    </div>
+  );  
+};
+
+export default User;
