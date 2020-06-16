85c7255b6437b4ddcfa2be6ddb8b2f4186938a8c
Azat777-user <mardanow.azat2016@yandex.ru>
2020-06-16 18:39:42 +0300

    add class name container

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,18 +1,39 @@
 import React from "react";
 import Paginator from "../common/Paginator/Paginator";
 import User from "./User";
-
+// import ReactPaginate from 'react-paginate';
 
 let Users = ({currentPage, totalUsersCount, sizePage, onPageChanged, users, ...props}) => {
-
+  //let countPages = Math.ceil(totalUsersCount / sizePage);
   return (
-    <div>
+    <div className="container">
+      { 
       <Paginator 
         currentPage={currentPage}
         onPageChanged={onPageChanged}
         totalItemsCount={totalUsersCount}
         sizePage={sizePage}
-      />
+      /> 
+      // <ReactPaginate
+      //   previousLabel={'<'}
+      //   nextLabel={'>'}
+      //   breakLabel={'...'}
+      //   breakClassName={'break-me'}
+      //   pageCount={countPages}
+      //   onPageChange={onPageChanged}
+      //   marginPagesDisplayed={2}
+      //   pageRangeDisplayed={5}
+      //   containerClassName={'pagination'}
+      //   activeClassName={'active'}
+      //   pageClassName={'page-item'}
+      //   pageLinkClassName={'page-link'}
+      //   previousClassName={'page-item'}
+      //   nextClassName={'page-item'}
+      //   previousLinkClassName={'page-link'}	
+      //   nextLinkClassName={'page-link'} 
+      // /> 
+     
+      }
       <div>
       {
         users.map(user => {
